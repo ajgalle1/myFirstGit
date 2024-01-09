28220cb33d5a476148a8de75431e755fce50ca53
Andrew <andrew.j.galle@outlook.com>
2024-01-09 10:42:26 -0500

    practice with enum

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace helloworld
+{
+    enum Make { Ford, Chevrolet, Toyota, Mazda };
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string VIN = "1G1A888";
+            int year = 1989;
+            Make make = Make.Ford;
+            string model = "Corsica";
+            double mileage = 1234.7;
+            bool isFourWheel = false;
+
+            Console.WriteLine(make);
+        }
+    }
+}
